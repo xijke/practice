2129f2a7e759ca169cccced274c7102cd30fd395
yan slukov <144355564+xijke@users.noreply.github.com>
2023-11-21 07:18:46 +0300

    Add files via upload

---
+++ Lab5_1/Lab5_1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Lab5_1
+{
+    partial class Lab5
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.txtY1 = new System.Windows.Forms.TextBox();
+            this.txtY2 = new System.Windows.Forms.TextBox();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 298);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(93, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "calculate z(x,y)";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(46, 51);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(100, 20);
+            this.txtX1.TabIndex = 1;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(194, 51);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(100, 20);
+            this.txtX2.TabIndex = 2;
+            // 
+            // txtY1
+            // 
+            this.txtY1.Location = new System.Drawing.Point(46, 126);
+            this.txtY1.Name = "txtY1";
+            this.txtY1.Size = new System.Drawing.Size(100, 20);
+            this.txtY1.TabIndex = 3;
+            // 
+            // txtY2
+            // 
+            this.txtY2.Location = new System.Drawing.Point(194, 126);
+            this.txtY2.Name = "txtY2";
+            this.txtY2.Size = new System.Drawing.Size(100, 20);
+            this.txtY2.TabIndex = 4;
+            // 
+            // lstResult
+            // 
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.Location = new System.Drawing.Point(174, 203);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(120, 95);
+            this.lstResult.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "x range";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "from:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "to:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "y range";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "from:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(168, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "to:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(133, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Result:";
+            // 
+            // Lab5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 333);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.txtY2);
+            this.Controls.Add(this.txtY1);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Lab5";
+            this.Text = "for loops";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TextBox txtY1;
+        private System.Windows.Forms.TextBox txtY2;
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab5_1/Lab5_1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_1
+{
+    public partial class Lab5 : Form
+    {
+        public Lab5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //добавляем переменные. parse преобразует строку из цифр в целое число
+            int fromX = int.Parse(txtX1.Text);
+            int toX = int.Parse(txtX2.Text);
+            int fromY = int.Parse(txtY1.Text);
+            int toY = int.Parse(txtY2.Text);
+
+            if (fromX > toX)
+            {
+                MessageBox.Show("Интервал должен быть от меньшего к большему(х)");
+                txtX1.Text = "";
+                txtX2.Text = "";
+            }
+            if(fromY > toY)
+            {
+                MessageBox.Show("Интервал должен быть от меньшего к большему(у)");
+                txtY1.Text = "";
+                txtY2.Text = "";
+            }
+            //вывод результата в listbox
+            for(int x = fromX; x <= toX; x++) 
+            {
+                for(int y = fromY; y <= toY; y++) 
+                {
+                    lstResult.Items.Add($"z(x,y) = {x} - {y} = {x - y}");
+                }
+            }
+        }
+    }
+}
